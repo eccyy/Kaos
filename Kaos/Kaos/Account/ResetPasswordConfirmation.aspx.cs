f8bf5da4a65837f830c586653d1a1cf001f3f692
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-02-13 13:00:18 +0100

    AspNetÄmIHaj

---
+++ Kaos/Kaos/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Kaos.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Kaos/Kaos/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kaos.Startup))]
+namespace Kaos
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
