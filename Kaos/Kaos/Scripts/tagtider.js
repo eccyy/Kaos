aef01f44f2ad55fb30452b276c5ffbb4e225c60a
jonatan.leanderz <jonatan.leanderz@KARR9027Y2M>
2017-02-20 14:46:42 +0100

    Get gud

--- Kaos/Kaos/Scripts/tagtider.js
+++ Kaos/Kaos/Scripts/tagtider.js
@@ -1,3 +1,21 @@
-﻿$.ajax("Någonstans här där det finns en server som kan kalla apin", {
-    method: "GET",
+﻿
+
+var url = 'http://api.tagtider.net/v1/stations.json';
+
+$("#tagtiderButton").click(function 
+    () {
+
+    var req = new digestAuthRequest('GET', url , 'tagtider', 'codemocracy');
+
+    req.request(function(data){
+        $("#textBox").text(data.value);
+    },function(err){
+        alert(err.text);
+    });
+
+   
+
 });
+
+
+
