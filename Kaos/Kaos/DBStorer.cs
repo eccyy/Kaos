a76ab5ce2407cba69dd44dac90ee02e38ac66b99
tomraj <tommy.rajert98@gmail.com>
2017-03-20 13:58:22 +0100

    lite DBStorer och TimeCalculator

---
+++ Kaos/Kaos/DBStorer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kaos
+{
+    public static class DBStorer
+    {
+        public static bool StoreTransfer(string id, string arrival, string newArrival, string train, string type, string comment)
+        {
+            int Id = int.Parse(id);
+            DateTime Arrival = DateTime.Parse(arrival);
+            DateTime NewArrival = DateTime.Parse(newArrival);
+            string Train = train;
+            string Type = type;
+            string Comment = comment;
+
+            //SendToDB(Id, Arrival, NewArrival,...)
+            return true; // Temporary // If DB was updated
+        }
+    }
+}
--- Kaos/Kaos/TimeCalculator.cs
+++ Kaos/Kaos/TimeCalculator.cs
@@ -5,8 +5,18 @@ using System.Web;
 
 namespace Kaos
 {
-    public class TimeCalculator
+    public static class TimeCalculator
     {
+        /// <summary>
+        /// Returns the all-time delay of a transfer in minutes
+        /// </summary>
+        /// <returns></returns>
+        public static double GetAllTimeTransferDelay(string transferId)
+        {
+            throw new NotImplementedException("I needs my DB guy to fix stuffs first ples");
 
+            // Send call to DB routine
+            // Adds together all delays with a sertain transfer id
+        }
     }
 }
